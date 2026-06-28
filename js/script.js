b247eb602232409d5a9c5fa5b81cd1ea811529cc
sakshamxpandey <sakshampandey536@gmail.com>
2026-06-28 13:21:28 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
+const hiddenElements = document.querySelectorAll(".hidden");
 
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        }
+
+    });
+});
+
+hiddenElements.forEach((el) => observer.observe(el));
