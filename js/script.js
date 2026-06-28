0f7cc251adffee1acac1df267d5a7a5e335410c2
sakshamxpandey <sakshampandey536@gmail.com>
2026-06-28 13:47:25 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -11,3 +11,26 @@ const observer = new IntersectionObserver((entries) => {
 });
 
 hiddenElements.forEach((el) => observer.observe(el));
+
+const software = [
+    "Premiere Pro",
+    "After Effects",
+    "DaVinci Resolve",
+    "Photoshop"
+];
+
+let index = 0;
+
+const softwareName = document.getElementById("software-name");
+
+setInterval(() => {
+
+    index++;
+
+    if(index >= software.length){
+        index = 0;
+    }
+
+    softwareName.textContent = software[index];
+
+},2000);
