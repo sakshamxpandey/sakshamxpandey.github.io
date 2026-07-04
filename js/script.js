56524a616d904c6f2d11592fcbabbe0b470a68a6
sakshamxpandey <sakshampandey536@gmail.com>
2026-07-04 10:43:09 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -18,3 +18,19 @@ const navLinks = document.querySelector(".nav-links");
 menuToggle.addEventListener("click", () => {
     navLinks.classList.toggle("active");
 });
+
+window.addEventListener("scroll", () => {
+
+    const navbar = document.querySelector(".navbar");
+
+    if(window.scrollY > 50){
+
+        navbar.classList.add("scrolled");
+
+    }else{
+
+        navbar.classList.remove("scrolled");
+
+    }
+
+});
